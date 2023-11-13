18c367b7ae3115aa718290dd394457e8afef8cd2
Nurek54 <g_sienkiewicz@o2.pl>
2023-11-13 17:44:43 +0100

    Modyfikacja zadania1

--- src/Zadanie1.java
+++ src/Zadanie1.java
@@ -18,26 +18,21 @@ public class Zadanie1 extends JFrame {
     private JTextArea textArea;
 
     public Zadanie1() {
-        // Ustawienia głównego okna JFrame
         setTitle("Aplikacja GUI");
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Dodajemy panel z komponentami do głównego okna
         JPanel panel = createPanel();
         add(panel);
 
-        // Wyśrodkuj okno na ekranie
         setLocationRelativeTo(null);
     }
 
     private JPanel createPanel() {
-        // Ustawienia panelu
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(4, 2, 10, 10));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        // Dodajemy komponenty do panelu
         panel.add(new JLabel("Wprowadź tekst:"));
         textField = new JTextField();
         panel.add(textField);
@@ -49,24 +44,19 @@ public class Zadanie1 extends JFrame {
         JButton button = new JButton("Wyświetl w JTextArea");
         panel.add(button);
 
-        // Inicjalizacja JTextArea i dodanie go wraz z JScrollPane
         textArea = new JTextArea();
         JScrollPane scrollPane = new JScrollPane(textArea);
         panel.add(scrollPane);
 
-        // Dodanie obsługi zdarzenia dla przycisku
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Pobierz tekst z JTextField i JPasswordField
                 String text = textField.getText();
                 char[] password = passwordField.getPassword();
 
-                // Wyświetl tekst w JTextArea
                 textArea.append("Tekst: " + text + "\n");
                 textArea.append("Hasło: " + new String(password) + "\n");
 
-                // Wyczyść pola tekstowe
                 textField.setText("");
                 passwordField.setText("");
             }
