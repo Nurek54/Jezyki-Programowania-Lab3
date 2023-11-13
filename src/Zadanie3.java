02d8e34457f070d1c2a76b8e600add72ade2106f
Nurek54 <g_sienkiewicz@o2.pl>
2023-11-13 17:43:43 +0100

    Modyfikacja zadania3

--- src/Zadanie3.java
+++ src/Zadanie3.java
@@ -5,9 +5,9 @@ import java.awt.event.ActionListener;
 
 /*Treść zadania:
 Stwórz okno dialogowe, które prosi użytkownika o podanie swojego imienia.
-Po kliknięciu "OK" wyświetl wiadomość powitalną w głównym oknie za pomocą
-JOptionPane lub niestandardowego okna dialogowego (JDialog). Upewnij się,
-że okno dialogowe można łatwo zamknąć, a wprowadzone imię może być wyświetlane w głównym oknie.
+Po kliknięciu "OK" wyświetl wiadomość powitalną w głównym oknie za pomocą JOptionPane
+lub niestandardowego okna dialogowego (JDialog). Upewnij się, że okno dialogowe można łatwo zamknąć,
+a wprowadzone imię może być wyświetlane w głównym oknie.
  */
 
 public class Zadanie3 extends JFrame {
@@ -15,33 +15,27 @@ public class Zadanie3 extends JFrame {
     private JLabel welcomeLabel;
 
     public Zadanie3() {
-        // Ustawienia głównego okna JFrame
         setTitle("Okno Dialogowe z Imieniem");
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Dodajemy panel z komponentami do głównego okna
         JPanel panel = createPanel();
         add(panel);
 
-        // Wyśrodkuj okno na ekranie
         setLocationRelativeTo(null);
     }
 
     private JPanel createPanel() {
-        // Ustawienia panelu
         JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
+        panel.setLayout(new BorderLayout());
         panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
-        // Dodajemy komponenty do panelu
         JButton showDialogButton = new JButton("Pokaż Okno Dialogowe");
-        panel.add(showDialogButton);
+        panel.add(showDialogButton, BorderLayout.NORTH);
 
         welcomeLabel = new JLabel("Witaj, ");
-        panel.add(welcomeLabel);
+        panel.add(welcomeLabel, BorderLayout.CENTER);
 
-        // Dodanie obsługi zdarzenia dla przycisku pokazującego okno dialogowe
         showDialogButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -53,22 +47,25 @@ public class Zadanie3 extends JFrame {
     }
 
     private void showNameInputDialog() {
-        // Tworzymy okno dialogowe
         JDialog nameDialog = new JDialog(this, "Podaj Imię", true);
         nameDialog.setSize(300, 150);
-        nameDialog.setLayout(new FlowLayout());
+        nameDialog.setLayout(new BorderLayout());
         nameDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 
-        // Dodajemy komponenty do okna dialogowego
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new FlowLayout());
+
         JLabel nameLabel = new JLabel("Imię:");
         JTextField nameField = new JTextField(20);
         JButton okButton = new JButton("OK");
 
-        nameDialog.add(nameLabel);
-        nameDialog.add(nameField);
-        nameDialog.add(okButton);
+        inputPanel.add(nameLabel);
+        inputPanel.add(nameField);
+        inputPanel.add(okButton);
 
-        // Dodanie obsługi zdarzenia dla przycisku OK
+        nameDialog.add(inputPanel, BorderLayout.CENTER);
+
+        // Dodanie obsługi dla przycisku OK
         okButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -82,15 +79,12 @@ public class Zadanie3 extends JFrame {
             }
         });
 
-        // Wyśrodkuj okno dialogowe na ekranie
         nameDialog.setLocationRelativeTo(this);
 
-        // Ustawienia widoczności okna dialogowego
         nameDialog.setVisible(true);
     }
 
     public static void main(String[] args) {
-        // Uruchomienie aplikacji w wątku obsługi zdarzeń
         SwingUtilities.invokeLater(() -> {
             Zadanie3 app = new Zadanie3();
             app.setVisible(true);
