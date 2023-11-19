159c1841a8a8ae7e78481d17d946e2c50fb250f3
Nurek54 <g_sienkiewicz@o2.pl>
2023-11-19 13:55:50 +0100

    Poprawa zadania2

--- src/Zadanie2.java
+++ src/Zadanie2.java
@@ -11,129 +11,87 @@ Możesz użyć JDialog lub JOptionPane do pobierania od użytkownika informacji 
  */
 
 public class Zadanie2 extends JFrame {
-
-    private JTextField textField;
-    private JTextArea textArea;
+    private JButton zmienKolorButton;
+    private JButton zmienRozmiarButton;
+    private JButton przesunButton;
+    private JButton resetButton;
+    private JLabel witajLabel;
+    private Point aktualnaPozycja;
 
     public Zadanie2() {
-        setTitle("Dynamiczna Zmiana Komponentów");
-        setSize(500, 400);
+        setTitle("Zadanie2 - Dynamiczne zmiany komponentów");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panel = createPanel();
-        add(panel);
+        witajLabel = new JLabel("PIWO", SwingConstants.CENTER);
+        add(witajLabel, BorderLayout.CENTER);
 
-        setLocationRelativeTo(null);
+        zmienKolorButton = new JButton("Zmień kolor");
+        zmienRozmiarButton = new JButton("Zmień rozmiar");
+        przesunButton = new JButton("Przesuń");
+        resetButton = new JButton("Reset");
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(zmienKolorButton);
+        buttonPanel.add(zmienRozmiarButton);
+        buttonPanel.add(przesunButton);
+        buttonPanel.add(resetButton);
+
+        add(buttonPanel, BorderLayout.SOUTH);
+
+        zmienKolorButton.addActionListener(e -> zmienKolor(witajLabel));
+        zmienRozmiarButton.addActionListener(e -> zmienRozmiar(witajLabel));
+        przesunButton.addActionListener(e -> przesunKomponent(witajLabel));
+        resetButton.addActionListener(e -> resetKomponent(witajLabel));
+
+        // Ustaw okno na pełny ekran
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        setResizable(true);
+
+        // Inicjalizacja aktualnej pozycji
+        aktualnaPozycja = witajLabel.getLocation();
     }
 
-    private JPanel createPanel() {
-        // Ustawienia panelu
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
-        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+    private void zmienKolor(Component komponent) {
+        Color wybranyKolor = JColorChooser.showDialog(this, "Wybierz kolor", komponent.getForeground());
+        if (wybranyKolor != null) {
+            komponent.setForeground(wybranyKolor);
+        }
+    }
 
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.insets = new Insets(5, 5, 5, 5);
+    private void zmienRozmiar(Component komponent) {
+        String nowyRozmiarStr = JOptionPane.showInputDialog(this, "Podaj nowy rozmiar (liczba):");
+        if (nowyRozmiarStr != null && !nowyRozmiarStr.isEmpty()) {
+            int nowyRozmiar = Integer.parseInt(nowyRozmiarStr);
+            Font aktualnaCzcionka = komponent.getFont();
+            komponent.setFont(new Font(aktualnaCzcionka.getName(), aktualnaCzcionka.getStyle(), nowyRozmiar));
+            // Ręczne przesunięcie etykiety na aktualne współrzędne
+            komponent.setLocation(aktualnaPozycja);
+        }
+    }
 
-        panel.add(new JLabel("Wprowadź tekst:"), gbc);
-        gbc.gridx = 1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        textField = new JTextField("Tekst");
-        panel.add(textField, gbc);
+    private void przesunKomponent(Component komponent) {
+        String noweXStr = JOptionPane.showInputDialog(this, "Podaj nową pozycję X (liczba):");
+        String noweYStr = JOptionPane.showInputDialog(this, "Podaj nową pozycję Y (liczba):");
 
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.fill = GridBagConstraints.NONE;
-        panel.add(new JLabel("Wprowadź obszar tekstu:"), gbc);
-        gbc.gridx = 1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        textArea = new JTextArea("Obszar tekstu");
-        panel.add(textArea, gbc);
+        if (noweXStr != null && !noweXStr.isEmpty() && noweYStr != null && !noweYStr.isEmpty()) {
+            int noweX = Integer.parseInt(noweXStr);
+            int noweY = Integer.parseInt(noweYStr);
+            komponent.setLocation(noweX, noweY);
+            // Aktualizacja aktualnej pozycji
+            aktualnaPozycja = new Point(noweX, noweY);
+        }
+    }
 
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.gridwidth = 2;
-        gbc.fill = GridBagConstraints.NONE;
-        JButton colorButton = new JButton("Zmień kolor");
-        JButton sizeButton = new JButton("Zmień rozmiar");
-        JButton positionButton = new JButton("Przesuń");
-        JButton resetButton = new JButton("Resetuj");
-
-        panel.add(colorButton, gbc);
-
-        gbc.gridy = 3;
-        panel.add(sizeButton, gbc);
-
-        gbc.gridy = 4;
-        panel.add(positionButton, gbc);
-
-        gbc.gridy = 5;
-        panel.add(resetButton, gbc);
-
-        // Dodanie obsługi dla przycisku zmiany koloru
-        colorButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Color selectedColor = JColorChooser.showDialog(Zadanie2.this, "Wybierz kolor", Color.BLACK);
-                if (selectedColor != null) {
-                    textField.setForeground(selectedColor);
-                    textArea.setForeground(selectedColor);
-                }
-            }
-        });
-
-        // Dodanie obsługi dla przycisku zmiany rozmiaru
-        sizeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String input = JOptionPane.showInputDialog(Zadanie2.this, "Podaj nowy rozmiar (np. 14):");
-                try {
-                    int newSize = Integer.parseInt(input);
-                    textField.setFont(textField.getFont().deriveFont((float) newSize));
-                    textArea.setFont(textArea.getFont().deriveFont((float) newSize));
-                } catch (NumberFormatException ex) {
-                    JOptionPane.showMessageDialog(Zadanie2.this, "Nieprawidłowy format rozmiaru.", "Błąd", JOptionPane.ERROR_MESSAGE);
-                }
-            }
-        });
-
-        // Dodanie obsługi dla przycisku zmiany pozycji
-        positionButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int newX = textField.getX() + 20;
-                int newY = textField.getY() + 20;
-                textField.setLocation(newX, newY);
-
-                int newAreaX = textArea.getX() - 20;
-                int newAreaY = textArea.getY() - 20;
-                textArea.setLocation(newAreaX, newAreaY);
-            }
-        });
-
-        // Dodanie obsługi przycisku reset
-        resetButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                textField.setForeground(Color.BLACK);
-                textArea.setForeground(Color.BLACK);
-                textField.setFont(new Font("Arial", Font.PLAIN, 12));
-                textArea.setFont(new Font("Arial", Font.PLAIN, 12));
-                textField.setLocation(0, 0);
-                textArea.setLocation(0, 0);
-            }
-        });
-
-        return panel;
+    private void resetKomponent(Component komponent) {
+        witajLabel.setText("PIWO");
+        komponent.setForeground(Color.BLACK);
+        komponent.setFont(new Font(komponent.getFont().getName(), Font.PLAIN, 12));
+        // Przywróć etykietę do ostatniej znanej pozycji
+        komponent.setLocation(aktualnaPozycja);
     }
 
     public static void main(String[] args) {
-        // Uruchomienie aplikacji w wątku obsługi zdarzeń
-        SwingUtilities.invokeLater(() -> {
-            Zadanie2 app = new Zadanie2();
-            app.setVisible(true);
-        });
+        SwingUtilities.invokeLater(() -> new Zadanie2().setVisible(true));
     }
 }
